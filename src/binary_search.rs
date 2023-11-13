a27777ecc796bb6f8797abf73fb2f19e02764720
Mateusz Nowak <mats.nowak@hotmail.com>
2023-11-14 00:14:10 +0100

    [Algomonster] Binary Search: general, find first matching, monotonic function

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -28,30 +28,46 @@ pub fn find_boundary_binary_search(sorted: &Vec<bool>) -> Option<usize> {
         if sorted[mid] {
             boundary_index = mid;
             found = true;
-            right = mid -1;
-        }
-        else {
+            right = mid - 1;
+        } else {
             left = mid + 1;
         }
     }
-    return if found { Some(boundary_index) } else { None }
+    return if found { Some(boundary_index) } else { None };
+}
+
+pub fn binary_search_first_matching<T: Ord>(input: &Vec<T>, matcher: impl Fn(&T) -> bool) -> Option<usize> {
+    let mut left: usize = 0;
+    let mut right: usize = input.len() - 1;
+    let mut first_matched_index = 0;
+    let mut found = false;
+
+    while left <= right {
+        let mid = (left + right).wrapping_div(2);
+        if matcher(&input[mid]) {
+            first_matched_index = mid;
+            found = true;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return if found { Some(first_matched_index) } else { None };
 }
 
 #[test]
 fn vanilla_binary_search_should_find() {
     let given_vector = vec![10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
     let given_target = 70;
-
     let result = vanilla_binary_search(&given_vector, given_target);
 
     assert_eq!(given_vector[result.unwrap()], given_target);
 }
 
 #[test]
-fn test_vanilla_binary_search_should_not_find() {
+fn vanilla_binary_search_should_not_find() {
     let given_vector = vec![10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
     let given_target = 25;
-
     let result = vanilla_binary_search(&given_vector, given_target);
 
     assert!(result.is_none())
@@ -60,7 +76,6 @@ fn test_vanilla_binary_search_should_not_find() {
 #[test]
 fn find_boundary_binary_search_should_find() {
     let given_input = vec![false, false, false, true, true, true, true];
-
     let result = find_boundary_binary_search(&given_input);
 
     assert_eq!(result.unwrap(), 3);
@@ -69,8 +84,23 @@ fn find_boundary_binary_search_should_find() {
 #[test]
 fn find_boundary_binary_search_given_only_false_should_not_find() {
     let given_input = vec![false, false, false, false, false];
-
     let result = find_boundary_binary_search(&given_input);
+
+    assert!(result.is_none());
+}
+
+#[test]
+fn binary_search_first_matching_should_find() {
+    let given_input = vec![false, false, false, true, true, true, true];
+    let result = binary_search_first_matching(&given_input, |x| *x == true);
+
+    assert_eq!(result.unwrap(), 3);
+}
+
+#[test]
+fn binary_search_first_matching_given_only_false_should_not_find() {
+    let given_input = vec![false, false, false, false, false];
+    let result = binary_search_first_matching(&given_input, |x| *x == true);
 
     assert!(result.is_none());
 }
