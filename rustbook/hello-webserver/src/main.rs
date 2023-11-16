92acf4a9639795fb6feac09404fee719a6aa33a2
Mateusz Nowak <mats.nowak@hotmail.com>
2023-11-16 23:21:42 +0100

    [Rustbook] hello-webserver return html

--- rustbook/hello-webserver/src/main.rs
+++ rustbook/hello-webserver/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
@@ -12,11 +13,19 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buff_reader = BufReader::new(stream);
+    let buff_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buff_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("index.html").unwrap();
+    let lenght = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {lenght}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
     println!("Request: {:#?}", http_request);
 }
