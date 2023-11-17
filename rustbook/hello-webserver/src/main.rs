8a8f78217e8ac14e795a0fe67302ced57f5a5c9d
Mateusz Nowak <mats.nowak@hotmail.com>
2023-11-18 00:35:33 +0100

    [Rustbook] handle GET or 404

--- rustbook/hello-webserver/src/main.rs
+++ rustbook/hello-webserver/src/main.rs
@@ -14,18 +14,18 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buff_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buff_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let http_request = buff_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("index.html").unwrap();
+    let (status_line, filename) = if http_request == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let lenght = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {lenght}\r\n\r\n{contents}");
-
     stream.write_all(response.as_bytes()).unwrap();
     println!("Request: {:#?}", http_request);
 }
