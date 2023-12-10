e1ab19be40b8ea3d24a7f10cdc6f4916480348eb
Mateusz Nowak <mats.nowak@hotmail.com>
2023-12-11 00:08:07 +0100

    [rustbook] minigrep -> working plus first test

--- rustbook/minigrep/src/lib.rs
+++ rustbook/minigrep/src/lib.rs
@@ -4,8 +4,23 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
 
-    println!("Within text:\n{contents}");
+    // println!("Within text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line)
+        }
+    }
+
+    result
 }
 
 pub struct Config {
@@ -20,5 +35,20 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
         Ok(Config { query, file_path })
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
--- rustbook/minigrep/src/main.rs
+++ rustbook/minigrep/src/main.rs
@@ -4,15 +4,11 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem with parsing arguments {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error {e}");
