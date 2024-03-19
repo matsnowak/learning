2ae7936c0eb238ac6b16c13cc66e4a33172204f3
Mateusz <mats.nowak@hotmail.com>
2024-03-19 23:49:35 +0100

    [too-many-list] Bad Stack full

---
+++ too-many-lists/README.md
@@ -0,0 +1,4 @@
+Code written during studying [Too-Many-Linked-Lists](https://rust-unofficial.github.io/too-many-lists/index.html)
+
+
+
---
+++ too-many-lists/lists/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod first;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn main_test() {}
+}
