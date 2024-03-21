26f4e0e704ba214ceac93ab1e18621cad5046037
Mateusz <mats.nowak@hotmail.com>
2024-03-21 22:49:59 +0100

    [too-many-list] An Ok Stack, Option

--- too-many-lists/lists/src/lib.rs
+++ too-many-lists/lists/src/lib.rs
@@ -1,19 +1,2 @@
 pub mod first;
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn main_test() {}
-}
+pub mod second;
