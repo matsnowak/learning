f93b7adcc92a0390087dcb9277d679e50080aa9a
Mateusz <mats.nowak@hotmail.com>
2024-03-23 00:11:16 +0100

    [too-many-lists] An Ok Stack, IntoIter

--- too-many-lists/lists/src/second.rs
+++ too-many-lists/lists/src/second.rs
@@ -17,6 +17,8 @@ impl<T> Default for List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: Link::None }
@@ -44,6 +46,17 @@ impl<T> List<T> {
 
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -101,5 +114,21 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter_test() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+
+        assert_eq!(iter.next(), None);
     }
 }
