7df085565fa0bfdf844cb690c857b4c2243ca69d
Mateusz <mats.nowak@hotmail.com>
2024-03-23 17:26:27 +0100

    [too-many-lists] An Ok Stack, IterMut

--- too-many-lists/lists/src/second.rs
+++ too-many-lists/lists/src/second.rs
@@ -21,6 +21,10 @@ pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: Link::None }
@@ -59,6 +63,12 @@ impl<T> List<T> {
             next: self.head.as_deref(),
         }
     }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
 }
 
 impl<T> Iterator for IntoIter<T> {
@@ -76,6 +86,17 @@ impl<'a, T> Iterator for Iter<'a, T> {
             // self.next = node.next.as_deref();
             self.next = node.next.as_ref().map::<&Node<T>, _>(|node| &node);
             &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
         })
     }
 }
@@ -169,5 +190,23 @@ mod test {
         assert_eq!(iter2.next(), Some(&3));
         assert_eq!(iter2.next(), Some(&2));
         assert_eq!(iter2.next(), Some(&1));
+    }
+    #[test]
+    fn iter_mut_test() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+
+        let mut iter2 = list.iter_mut();
+        assert_eq!(iter2.next(), Some(&mut 3));
+        assert_eq!(iter2.next(), Some(&mut 2));
+        assert_eq!(iter2.next(), Some(&mut 1));
     }
 }
