c16d3cb53bf5fa6543fe10dccb4f7a1b6b7b3756
Mateusz <mats.nowak@hotmail.com>
2024-03-28 23:40:28 +0100

    [too-many-lists] A Bad Safe Deque, Building -> not compiling

--- too-many-lists/lists/src/fourth.rs
+++ too-many-lists/lists/src/fourth.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{borrow::BorrowMut, cell::RefCell, rc::Rc};
 
 pub struct List<T> {
     head: Link<T>,
@@ -11,4 +11,45 @@ struct Node<T> {
     elem: T,
     next: Link<T>,
     prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+
+        match self.head.take() {
+            Some(old_head) => {
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
