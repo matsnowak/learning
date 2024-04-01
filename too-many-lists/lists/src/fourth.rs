8fe413ec784a418a0d5b040d3eec635aab55f133
Mateusz <mats.nowak@hotmail.com>
2024-04-01 22:47:02 +0200

    [too-many-lists] A Bad Safe Deque, Breaking, Peek

--- too-many-lists/lists/src/fourth.rs
+++ too-many-lists/lists/src/fourth.rs
@@ -1,4 +1,7 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{
+    cell::{Ref, RefCell},
+    rc::Rc,
+};
 
 pub struct List<T> {
     head: Link<T>,
@@ -60,6 +63,18 @@ impl<T> List<T> {
             Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
         })
     }
+
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head
+            .as_ref()
+            .map(|node| Ref::map(node.borrow(), |node| &node.elem))
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -93,5 +108,17 @@ mod test {
 
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn peek_test() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        assert_eq!(&*list.peek_front().unwrap(), &3);
     }
 }
