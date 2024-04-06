630c8bf982dae979af79c5b25d4460ea2d90f3ab
Mateusz <mats.nowak@hotmail.com>
2024-04-06 09:56:01 +0200

    [too-many-lists] An Ok Unsafe Queue, Unsafe

--- too-many-lists/lists/src/fifth.rs
+++ too-many-lists/lists/src/fifth.rs
@@ -1,4 +1,4 @@
-use std::mem;
+use std::{mem, ptr};
 
 pub struct List<T> {
     head: Link<T>,
@@ -21,32 +21,31 @@ impl<T> List<T> {
     }
 
     pub fn push(&mut self, elem: T) {
-        let new_tail = Box::new(Node {
+        let mut new_tail = Box::new(Node {
             elem: elem,
             next: None,
         });
 
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                old_tail.next = Some(new_tail);
-                old_tail.next.as_deref_mut()
-            }
-            None => {
-                self.head = Some(new_tail);
-                self.head.as_deref_mut()
-            }
-        };
+        let raw_tail: *mut _ = &mut *new_tail;
 
-        self.tail = new_tail;
+        if !self.tail.is_null() {
+            unsafe {
+                (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            self.head = Some(new_tail);
+        }
+
+        self.tail = raw_tail;
     }
 
-    pub fn pop(&'a mut self) -> Option<T> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|head| {
             let head = *head;
             self.head = head.next;
 
             if self.head.is_none() {
-                self.tail = None;
+                self.tail = ptr::null_mut();
             }
 
             head.elem
@@ -78,6 +77,13 @@ mod test {
         assert_eq!(list.pop(), Some(4));
 
         assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        list.push(6);
+        list.push(7);
+
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
         assert_eq!(list.pop(), None);
     }
 }
