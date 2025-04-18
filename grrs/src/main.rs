6fea681931a582dc51864f73cb56c1979bc0632f
Mateusz Nowak <mats.nowak@hotmail.com>
2025-04-19 00:50:41 +0200

    [grrs] add test

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,27 +1,50 @@
-use std::io::{BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Write};
 
 use anyhow::{Context, Result};
 use clap::Parser;
-
+use log::info;
 #[derive(Parser)]
 struct CliArgs {
     pattern: String,
     path: std::path::PathBuf,
 }
 fn main() -> Result<()> {
+    env_logger::init();
+    info!("starting grrs");
     let args = CliArgs::parse();
     println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 
     let file = std::fs::File::open(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
     let reader = BufReader::new(file);
+    find_matches(reader, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
+
+fn find_matches(reader: impl BufRead, pattern: &str, mut writer: impl Write) {
     reader.lines().for_each(|line| {
         if let Ok(line_internal) = line {
-            if line_internal.contains(&args.pattern) {
-                println!("{line_internal}");
+            if line_internal.contains(pattern) {
+                writeln!(writer, "{line_internal}");
             }
         }
     });
+}
+#[cfg(test)]
+mod test {
+    use std::io::{BufReader, Cursor};
 
-    Ok(())
+    use crate::find_matches;
+
+    #[test]
+    fn should_find_matches() {
+        let mut result = Vec::new();
+        let input = ["lorem ipsum\n", "dolor sit amet\n"].concat();
+        let reader = BufReader::new(Cursor::new(input));
+        find_matches(reader, "lorem", &mut result);
+
+        let output = vec![String::from_utf8(result).unwrap()];
+        assert_eq!(output, vec!["lorem ipsum\n"]);
+    }
 }
